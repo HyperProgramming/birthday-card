2ed9a5fcbbee6600b1ebecc58f9227bd300d3ebc
HyperProgramming <jrdcprogrammings@gmail.com>
2025-08-14 17:20:40 +0800

    Delete Birthday directory

--- Birthday/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const card = document.querySelector(".birthdayCard");
-    card.addEventListener("click", function() {
-        card.classList.toggle("flipped");
-    });
-});
